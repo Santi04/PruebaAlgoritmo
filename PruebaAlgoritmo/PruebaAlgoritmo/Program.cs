a6bea24dd191c5fd6a26b7e97d173300d659b233
santi04 <santivelas5@gmail.com>
2023-08-28 12:04:16 -0500

    división número mayor

--- PruebaAlgoritmo/PruebaAlgoritmo/Program.cs
+++ PruebaAlgoritmo/PruebaAlgoritmo/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class algortimo
+{
+
+    static void Main(string[] args)
+    {
+
+        //declaración variables
+        int num1, num2, division;
+
+        Console.WriteLine("Ingrese el número 1: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el número 2: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        //proceso
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("El número mayor es: " + num1);
+            division = (num1 / num2);
+
+        }
+        else
+        {
+
+            Console.WriteLine("El número mayor es: " + num2);
+            division = (num2 / num1);
+
+        }
+        //datos de salida
+        Console.WriteLine("El resultado de la división es: " + division);
+
+    }
+
+}
