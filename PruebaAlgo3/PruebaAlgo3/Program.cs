43a2f818db94a6582e1f2777cb509ca662f8acff
santi04 <santivelas5@gmail.com>
2023-08-28 12:48:46 -0500

    Desarrollo algoritmo 6

---
+++ PruebaAlgo3/PruebaAlgo3/Program.cs
@@ -0,0 +1,39 @@
+﻿//Construir un programa que pida por pantalla 3 números y luego diga cuál es el mayor de los números ingresados
+
+public class mayor
+{
+    static void Main(string[] args)
+    {
+        //declaración variables
+        int n1, n2, n3, mayor;
+
+        Console.WriteLine("Ingrese el número 1: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el número 2: ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el número 3: ");
+        n3 = Convert.ToInt32(Console.ReadLine());
+
+        //proceso
+        if(n1 > n2 && n1 > n3) 
+        {
+            mayor = n1;
+        //Console.WriteLine("El número mayor es: " + mayor);
+
+        }else if(n2 > n1 && n2 > n3)
+        {
+            mayor = n2;
+          //  Console.WriteLine("El número mayor es: " + mayor);
+        }
+        else
+        {
+            mayor = n3;
+          //  Console.WriteLine("El número mayor es: " + mayor);
+        }
+
+        Console.WriteLine("El número mayor es: " + mayor);
+
+    }
+}
