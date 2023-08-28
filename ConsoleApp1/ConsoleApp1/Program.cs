99e715276d4fd30bc80826cd4f49795d60f5ed74
santi04 <santivelas5@gmail.com>
2023-08-28 12:24:21 -0500

    Desarrollo Algoritmo 4 taller

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿public class numPar
+{
+    static void Main(string[] args)
+    {
+        //declarar variables
+        int num, cociente;
+
+        Console.WriteLine("Ingrese un número; ");
+        num = Convert.ToInt32(Console.ReadLine());
+        cociente = num % 2;
+
+        //proceso
+        if (cociente == 0 )
+        {
+            Console.WriteLine("El número es par ");
+
+        }
+        else
+        {
+
+            Console.WriteLine("El número es impar ");
+        }
+
+    }
+
+}
