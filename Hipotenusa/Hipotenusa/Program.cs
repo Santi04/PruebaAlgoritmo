fa4a1e6719c9f49494d8cf134e39159dc7dfc105
santi04 <santivelas5@gmail.com>
2023-08-30 16:44:55 -0500

    algoritmo 5 taller

---
+++ Hipotenusa/Hipotenusa/Program.cs
@@ -0,0 +1,24 @@
+﻿//Dados los catetos de un triángulo rectángulo, calcular su hipotenusa
+public class hipotenusa
+{
+    static void Main(string[] args)
+    {
+        //declaración variables
+        double cateto1, cateto2, hipotenusa;
+
+        //entrada
+        Console.WriteLine("Ingrese el cateto #1: ");
+        cateto1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el cateto #2: ");
+        cateto2 = Convert.ToDouble(Console.ReadLine());
+
+        //proceso
+        hipotenusa = Math.Sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+
+
+        //salida
+        Console.WriteLine("La hipotenusa es: " + hipotenusa);
+    }
+
+}
