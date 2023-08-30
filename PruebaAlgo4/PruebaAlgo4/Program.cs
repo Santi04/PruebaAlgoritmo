c9361a941998185d18ff4b83d13a6d090726430f
santi04 <santivelas5@gmail.com>
2023-08-30 16:08:08 -0500

    Desarrollo algo 6 taller

---
+++ PruebaAlgo4/PruebaAlgo4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
+
+public class conversorgrados
+{
+    static void Main(string[] args)
+    {
+        //delcaración var
+        double celcius, farenheit;
+
+        //entrada
+        Console.WriteLine("Ingrese grados °F que desea convertir a °C: ");
+        farenheit = Convert.ToDouble(Console.ReadLine());
+
+        //proceso
+
+        celcius = (((farenheit - 32)*5)/9);
+
+
+        //datos de salida
+        Console.WriteLine(farenheit + "°F son: " + celcius + "°C");
+    }
+
+
+}
