5d8d43297931c552a7156d43b3cabc99f0fd8b96
santi04 <santivelas5@gmail.com>
2023-08-30 17:15:19 -0500

    Ejemplo menu try/cath

---
+++ Menu/Menu/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Ejemplo_menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool salir = false;
+
+            while (!salir)
+            {
+
+                try
+                {
+
+                    Console.WriteLine("1. Opción 1");
+                    Console.WriteLine("2. Opción 2");
+                    Console.WriteLine("3. Opción 3");
+                    Console.WriteLine("4. Salir");
+                    Console.WriteLine("Elige una de las opciones");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Has elegido la opción 1");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Has elegido la opción 2");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Has elegido la opción 3");
+                            break;
+                        case 4:
+                            Console.WriteLine("Has elegido salir de la aplicación");
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Elige una opcion entre 1 y 4");
+                            break;
+                    }
+
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
