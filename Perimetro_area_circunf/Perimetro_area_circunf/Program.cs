41c37bc29f4505d022de30554dbd4fc2b6ddb269
santi04 <santivelas5@gmail.com>
2023-08-30 17:08:27 -0500

    proceso

--- Perimetro_area_circunf/Perimetro_area_circunf/Program.cs
+++ Perimetro_area_circunf/Perimetro_area_circunf/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Realizar un algoritmo C# donde me permita calcular el perímetro/circunferencia y área de las siguientes
+figuras geométricas: cuadrado, rectángulo, triángulo y círculo.
+Mostrar en pantalla los valores calculados por cada figura geométrica.*/
+
+public class pitagoras
+{
+    static void Main(string[] args)
+    {
+        //declaración variables
+        double catetoA, catetoB;
+
+        /*datos de entrada
+         para el circulo se necesita diametro y radio
+         */
+
+        //proceso
+
+        //salida
+
+    }
+
+}
