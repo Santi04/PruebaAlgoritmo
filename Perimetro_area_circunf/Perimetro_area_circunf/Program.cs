03a151b643172603d494c124cf64282a0f744fce
santi04 <santivelas5@gmail.com>
2023-08-30 21:22:09 -0500

    Avances significativo

--- Perimetro_area_circunf/Perimetro_area_circunf/Program.cs
+++ Perimetro_area_circunf/Perimetro_area_circunf/Program.cs
@@ -7,15 +7,60 @@ public class pitagoras
     static void Main(string[] args)
     {
         //declaración variables
-        double catetoA, catetoB;
+        double ladoCuadrado, periCuadrado, areaCuadrado, baseRec, altRec, periRec, areaRec, lado1Triangulo, lado2Triangulo,
+            lado3Triangulo, altTriangulo, periTriangulo, areaTriangulo, radioCir, periCir, areaCir;
 
         /*datos de entrada
-         para el circulo se necesita diametro y radio
+         para el circulo se necesita radio
+        para el cuadro 1 lado
+        rectangulo base y altura
+        triangulo 3 la
          */
 
+        Console.WriteLine("Ingrese un lado del cuadrado: ");
+        ladoCuadrado = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la base del rectangulo: ");
+        baseRec = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la altura del rectangulo: ");
+        altRec = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el lado 1 del triangulo: ");
+        lado1Triangulo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el lado 2 del triangulo: ");
+        lado2Triangulo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el lado 3 del triangulo: ");
+        lado3Triangulo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la altura del triangulo: ");
+        altTriangulo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el radio del circulo: ");
+        radioCir = double.Parse(Console.ReadLine());
+
         //proceso
+        periCuadrado = ladoCuadrado * 4;
+        areaCuadrado = ladoCuadrado * 2;
+        periRec = (baseRec * 2 + altRec * 2);
+        areaRec = baseRec * altRec;
+        areaTriangulo = (lado1Triangulo * altTriangulo) / 2;
+        periTriangulo = lado1Triangulo + lado2Triangulo + lado3Triangulo;
+        periCir = Math.PI * 2 * radioCir;
+        areaCir = Math.PI * Math.Pow(radioCir, 2);
 
         //salida
+
+        Console.WriteLine("El perimetro del cuadrado es: " + periCuadrado);
+        Console.WriteLine("El arear del cuadrado es: " + areaCuadrado);
+        Console.WriteLine("El perimetro del rectangulo es: " + periRec);
+        Console.WriteLine("El area del rectangulo es: " + periRec);
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
 
     }
 
